9203285ab104c908db90fd54374e45932e2b5622
yasuo masuyama <y.m19980526@gmail.com>
2023-05-23 12:40:19 +0900

    CRUD機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :pictures
   root to: 'users#index'
   resources :sessions, only:[:new, :create, :destroy]
   resources :users, only:[:new, :create, :show]
